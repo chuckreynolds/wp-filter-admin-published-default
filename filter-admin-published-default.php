d1a3cc337348e0de94c809aa8f4a59dce6e99849
ChuckReynolds <chuck@vuurr.com>
2014-07-29 15:45:35 -0700

    add gpl stuff, index file for safe sex, and spacing

--- filter-admin-published-default.php
+++ filter-admin-published-default.php
@@ -1,24 +1,41 @@
 <?php
 /*
- * Plugin Name: Filter Admin Published Default
- * Version: 1.0
- * Plugin URI: https://gist.github.com/chuckreynolds/0fa3af52462b5929e74d
- * Description: Enables the Pages and Posts links in admin to show the Published filter by default
- * Author: Pigs Eating Hotdogs
- * Author URI: https://github.com/chuckreynolds/wp-filter-admin-published-default
- * Requires at least: 3.8
- * Tested up to: 4.0
- */
+Plugin Name: Filter Admin Published Default
+Version: 1.0
+Plugin URI: https://gist.github.com/chuckreynolds/0fa3af52462b5929e74d
+Description: Enables the Pages and Posts links in admin to show the Published filter by default
+Author: Pigs Eating Hotdogs
+Author URI: https://github.com/chuckreynolds/wp-filter-admin-published-default
+Requires at least: 3.8
+Tested up to: 4.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright 2014 Internet
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 add_action ( 'admin_menu', 'rkv_filter_admin_published_default' );
 
 function rkv_filter_admin_published_default() {
 	// call global submenu item
 	global $submenu;
-	
+
 	// edit main link for posts
 	$submenu['edit.php'][5][2] = 'edit.php?post_status=publish';
 
 	// edit main link for pages
 	$submenu['edit.php?post_type=page'][5][2] = 'edit.php?post_type=page&post_status=publish';
-    
+
 }
--- readme.txt
+++ readme.txt
@@ -1,18 +1,20 @@
 === Filter Admin Published Default ===
 Contributors: ryno267, norcross
-Donate link: 
+Donate link:
 Tags: wordpress, admin, published, edit link, posts edit, pages edit
 Requires at least: 3.8
 Tested up to: 4.0
 Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Enables the Pages and Posts links in admin to show the Published filter by default
 
 == Description ==
 
 Enables the Pages and Posts links in admin to show the Published filter by default.
-Came out of a need as clients have a lot of pre-scheduled posts so a LOT of drafts are up 
-top and I got tired of the extra click. Tweeted for ideas, @Norcross answered and whipped 
+Came out of a need as clients have a lot of pre-scheduled posts so a LOT of drafts are up
+top and I got tired of the extra click. Tweeted for ideas, @Norcross answered and whipped
 this function up quick.
 
 == Usage ==
@@ -28,7 +30,7 @@ Installing "Filter Admin Published Default" can be done either by searching for 
 1. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Frequently Asked Questions ==
-
+1. How do it work? Just install, activate and you're good!
 
 == Changelog ==
 
