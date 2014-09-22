2bc7a7e81b156827c975f7cde2acf88a1778b061
Ernesto Méndez <mendezcode@gmail.com>
2014-09-22 10:57:44 -0400

    add filter to filtered post types

--- filter-admin-published-default.php
+++ filter-admin-published-default.php
@@ -32,8 +32,8 @@ function rkv_filter_admin_published_default() {
 	// call global submenu item
 	global $submenu;
 	
-	// post types to filter, add more post types here
-	$post_types = array('page');
+	// post types to filter
+	$post_types = apply_filters('rkv_filter_post_types', array('page'));
 
 	// edit main link for posts
 	$submenu['edit.php'][5][2] = 'edit.php?post_status=publish';
